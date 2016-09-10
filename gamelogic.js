cb12f311c69411f52967636292e27e94e173a98c
Colten Jackson <Colten Jackson>
2016-09-10 01:54:26 -0400

    pretty happy with this. messy javascript.

--- gamelogic.js
+++ gamelogic.js
@@ -1,5 +1,4 @@
 
-var latestguess;
 var arrowSelected = false;
 var arrowElement = document.getElementById("arrow");
 var sliderElement = document.getElementById("slider");
@@ -12,21 +11,30 @@ sliderElement.addEventListener('mousedown', function(e){
 
 document.body.addEventListener('mouseup', function(e){
   if(arrowSelected){
-    latestguess = Math.floor((Number(arrowElement.style.left.slice(0,-2)) + 15)/ 6);
+    makeAGuess(Math.floor((Number(arrowElement.style.left.slice(0,-2)) + 15)/ 6));
+
+  }
+})
+
+function makeAGuess(latestguess){
     var magnitudeOfWrongness = checkGuess(latestguess);
     console.log(magnitudeOfWrongness);
     if(magnitudeOfWrongness === 0){
         arrowElement.style.borderBottomColor = "rgb(0,255,0)";
+        guesstext.style.borderColor = "rgb(0,255,0)";
     } else {
         var red = (255 - Math.floor(magnitudeOfWrongness * 255));
         var blue = Math.floor(magnitudeOfWrongness * 255);
         arrowElement.style.borderBottomColor = "rgb("+red+",0,"+blue+")";
+        guesstext.style.borderColor = "rgb("+red+",0,"+blue+")";
     }
+    document.getElementById('guesstext').value = latestguess;
+
     attachGuessArrow();
     arrowSelected = false;
     //displayGuess();
-  }
-})
+}
+
 
 function attachGuessArrow(){
   var newArrow = arrowElement.cloneNode(false)
@@ -34,20 +42,38 @@ function attachGuessArrow(){
   sliderElement.insertBefore(newArrow, arrowElement);
 }
 
+document.getElementById('guessbutton').addEventListener('click', function(){
+  var guessVal = document.getElementById('guesstext').value;
+  if(guessVal >= 0 && guessVal <= 100){
+    moveArrow(-15 + (guessVal * 6));
+  }
+  makeAGuess(guessVal);
+})
 
 document.body.addEventListener('mousemove', function(e){
   if(arrowSelected){
-    var newpos = Number(arrowElement.style.left.slice(0,-2)) + e.movementX;
-    if(newpos >= -15 && newpos <= 585){
-      arrowElement.style.left = newpos + "px";
-      arrowElement.childNodes[1].textContent = Math.floor((newpos + 15) / 6);
-    }
+    moveArrow(Number(arrowElement.style.left.slice(0,-2)) + e.movementX);
   }
 })
 
+function moveArrow(newpos){
+  if(newpos >= -15 && newpos <= 585){
+      arrowElement.style.left = newpos + "px";
+      arrowElement.childNodes[1].textContent = Math.floor((newpos + 15) / 6);
+    }
+}
+
+
+document.getElementById('guesstext').addEventListener('keydown', function(e){
+  if(e.key == "Enter"){
+    document.getElementById('guessbutton').click();
+  }
+  console.log(e);
+
+})
 
 function matchGenerator(){ //returns a function that takes a number as an argument.
-                           //the distance of the guess from the random number enclosed is returned as a ratio
+//the distance of the guess from the random number enclosed is returned as a ratio
                            // 0 means a match, 1 means you couldn't be more wrong. Closer to 0 = getting warmer.
   var numberToMatch = Math.floor(Math.random() * 100);
   return function(numberToCheck){
@@ -61,3 +87,11 @@ function matchGenerator(){ //returns a function that takes a number as an argume
     }
   }
 }
+
+document.getElementById('resetbutton').addEventListener('click', function(){
+  document.getElementById('slider').innerHTML = '<div id="arrow" class="arrow" style="left: -15; border-bottom: 20px solid #888888;"><p>0</p></div>'
+  document.getElementById('guesstext').value = '';
+  document.getElementById('guesstext').style.borderColor = "black";
+})
+
+
